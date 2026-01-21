45f5a31d689f7c1b260686f450bba33783e42c0a
asif <mohammadasifali049@gmail.com>
2026-01-21 17:02:17 +0530

    header add

---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import HeaderItem from "./HeaderItem";
+import logo from "../assets/logo.png";
+import { HiPlus, HiStar, HiHome, HiDotsVertical } from "react-icons/hi";
+import { HiMagnifyingGlass, HiPlayCircle } from "react-icons/hi2";
+import { MdLiveTv } from "react-icons/md";
+import profileIcon from "../assets/profile-icon.png";
+
+function Header() {
+  const [toggle, setToggle] = useState(true);
+
+  const menu = [
+    { icon: HiHome, name: "Home" },
+    { icon: HiMagnifyingGlass, name: "Search" },
+    { icon: HiPlus, name: "Watchlist" },
+    { icon: HiStar, name: "Originals" },
+    { icon: HiPlayCircle, name: "Movies" },
+    { icon: MdLiveTv, name: "Series" },
+  ];
+
+  return (
+    <div className="flex items-center  justify-between bg-white p-5">
+      <div className="flex gap-8 items-center">
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-[80px] md:w-[100px] object-cover"
+        />
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />,
+          )}
+        </div>
+        <div className="md:hidden" onClick={() => setToggle(!toggle)}>
+          <HeaderItem name={" "} Icon={HiDotsVertical} />
+          {toggle ? (
+            <div className="absolute mt-3 bg-[#ffffff] border-[1px] border-gray-700 p-3 px-5 py-4">
+              {menu.map(
+                (item, index) =>
+                  index > 2 && <HeaderItem name={item.name} Icon={item.icon} />,
+              )}
+            </div>
+          ) : null}
+        </div>
+      </div>
+      <img
+        src={profileIcon}
+        alt="Profile"
+        className="w-[40px] h-[40px] rounded-full"
+      />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/HeaderItem.jsx
@@ -0,0 +1,10 @@
+function HeaderItem({ name, Icon }) {
+  return (
+    <div className=" flex items-center gap-2 cursor-pointer text-[18px] font-semibold course-pointer hover:underline underline-offset-8 mb-2">
+      <Icon />
+      <h2 className="text-black">{name}</h2>
+    </div>
+  );
+}
+
+export default HeaderItem;
